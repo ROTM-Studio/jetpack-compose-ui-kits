0c16ef8e1eba9a5c0f9fb99a9578d833c61661bf
Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>
2021-11-10 15:36:51 +0800

    Modify Nft data

--- SeaSellCryptoAppUIKit/app/src/main/java/com/rotmstudio/seasellcryptoappuikit/data/Nft.kt
+++ SeaSellCryptoAppUIKit/app/src/main/java/com/rotmstudio/seasellcryptoappuikit/data/Nft.kt
@@ -5,7 +5,7 @@ import com.rotmstudio.seasellcryptoappuikit.R
 data class Nft(
     val id: Long,
     val isTrending: Boolean,
-    val favorite: Boolean,
+    val isFavorite: Boolean,
     val image: Int,
     val name: String,
     val description: String
@@ -15,15 +15,15 @@ val nfts = listOf(
     Nft(
         id = 1L,
         isTrending = false,
-        favorite = false,
+        isFavorite = true,
         image = R.drawable.image_1,
         name = "Fast Food",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius cursus placerat."
     ),
     Nft(
         id = 2L,
-        isTrending = false,
-        favorite = false,
+        isTrending = true,
+        isFavorite = false,
         image = R.drawable.image_2,
         name = "Ethereum",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius cursus placerat."
@@ -31,15 +31,15 @@ val nfts = listOf(
     Nft(
         id = 3L,
         isTrending = false,
-        favorite = false,
+        isFavorite = true,
         image = R.drawable.image_3,
         name = "Dark Game",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius cursus placerat."
     ),
     Nft(
         id = 4L,
-        isTrending = false,
-        favorite = false,
+        isTrending = true,
+        isFavorite = false,
         image = R.drawable.image_4,
         name = "Warehouse",
         description = "Menggambarkan betapa pentingnya sebuah persediaan"
